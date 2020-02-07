c8ca555c3fdc1d941ee1c6aff4d908be1e3b6c2e
WayneHo25 <wayneho18@gmail.com>
2020-02-08 02:24:32 +1100

    Create new master

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,22 +1,12 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-import configureStore from 'store/ConfigureStore';
+ReactDOM.render(<App />, document.getElementById('root'));
 
-import "assets/scss/material-kit-pro-react.scss";
-import App from 'containers/App';
-import registerServiceWorker from 'registerServiceWorker';
-
-const store = configureStore();
-
-render (
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
-);
-registerServiceWorker();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
