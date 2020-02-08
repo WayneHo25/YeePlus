971cac172b87cf650625a9d3cac1fbd54d7c5699
WayneHo25 <wayneho18@gmail.com>
2020-02-09 00:18:15 +1100

    Update package.json

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import configureStore from 'store/ConfigureStore'
 
 import 'assets/scss/material-kit-pro-react.scss'
 import App from 'containers/App'
-import * as serviceWorker from './serviceWorker'
+import registerServiceWorker from 'registerServiceWorker';
 
 const store = configureStore()
 
@@ -19,7 +19,4 @@ render(
   </Provider>,
   document.getElementById('root')
 )
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register()
+registerServiceWorker();
