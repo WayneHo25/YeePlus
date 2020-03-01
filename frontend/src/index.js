38e5139e9d7d7c406cba991162c49a54628f1ff4
WayneHo25 <wayneho18@gmail.com>
2020-03-01 11:05:36 +1100

    Change to RashRouter

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
 import configureStore from 'store/ConfigureStore'
@@ -13,9 +13,9 @@ const store = configureStore()
 
 render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
   document.getElementById('root')
 )
